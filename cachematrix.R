bf336c6450a807dc652ca25221235beb286c3d50
billthecat2 <chris@jmfx.com>
2015-10-25 13:04:47 -0400

    Update cachematrix.R
    Added comments to the code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions in this assignment give an example of 
+## caching time-consuming computations. The makeCacheMatrix
+## function creates a matrix, calculates its inverse, and
+## stores the inverse for future reference. The cacheSolve
+## function retrieves the cached inverse if it exists, 
+## otherwise, it invokes the makeCacheMatrix function's 
+## setinverse subfunction.
 
-## Write a short comment describing this function
+## makeCacheMatrix takes as its arguments an integer vector,
+## with the row and column count. This funciton assumes 
+## the input matrix is invertible. That is, it is a suaare
+## matrix and its determinant is not zero. makeCacheMatrix
+## has four subfunctions and a variable:
+## 1. m - variable that contains either NULL or the input
+##    matrix's inverse.
+## 1. set() - Creates a matrix using the arguments passed 
+##   in from the makeCacheMatrix function. Running set()
+##    will initialize the variable m to NULL.
+## 2. get() - Returns the input matrix
+## 3. setinverse() - Calculates the inverse of the input
+##    matrix using the solve() function and saves the 
+##    result to m stored in makeCacheMatrix.  
+##    setinverse() will exit with an error if the matrix 
+##    cannot be inverted.
+##  4. getinverse() - Returns the input matrix's inverse.
+##    If the inverse has not been calculated, returns NULL.
 
 makeCacheMatrix <- function(x = numeric()) {
 
@@ -22,7 +44,12 @@ makeCacheMatrix <- function(x = numeric()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes as its argument the matrix created by
+## makeCacheMatrix() and checks the value of the makeCacheMatrix()
+## variable m. If m contains the value of the inverse
+##  (i.e. if m is not NULL), then m is returned. If m is NULL,
+##  then cacheSolve invokes makeCacheMatrix$setinverse 
+##  and returns the computed value.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
